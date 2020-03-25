ddda1e0ad286f8463a4f25b0f7e72b2c1c878548
xiaobo <844854626@qq.com>
2020-03-26 01:15:50 +0800

    测试

---
+++ test/src/com/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.test;
+
+/**
+ * @author xiaobo
+ * @create 2020-03-26 1:15
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld ");
+    }
+}
